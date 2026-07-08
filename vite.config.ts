bbdf3cd4b31065196e2c905df7bd6763e6efa9fa
Noor-Aldin1 <nooraldin.aboalsaid@gmail.com>
2026-07-08 22:04:45 +0300

    chore: set base path to relative in vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // أضف هذا السطر هنا
   server: {
     host: "::",
     port: 8080,
